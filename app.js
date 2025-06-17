51b8d927da1a64303d2af8202ef3c3a08f28eab5
Arthur Caetano <arthur.cateano.da.silva01@gmail.com>
2025-06-16 22:12:20 -0300

    alterando limite

---
+++ app.js
@@ -0,0 +1,22 @@
+// Exibe uma mensagem no console
+console.log("Olá, mundo!");
+
+// Declaração de variáveis
+let nome = "Arthur";
+let idade = 25;
+
+// Condicional (if)
+if (idade >= 18) {
+  console.log(nome + " é maior de idade.");
+} else {
+  console.log(nome + " é menor de idade.");
+}
+
+// Função simples
+function saudacao(nome) {
+  return "Olá, " + nome + "!";
+}
+
+// Chamando a função
+console.log(saudacao("Maria"));
+
--- code_1.py
+++ code_1.py
@@ -1,2 +1,16 @@
 print('Hello Mundo')
+from random import randint
+from time import sleep
+computador = randint(0,5) # Faz o computador 'PENSAR'
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 a 5. Tente aidvinhar...')
+print('-=-' * 20)
+jogador = int(input('Em que número pensei? ')) # Jogaodr tenta adivinhar
+print('PROCESSANDO...')
+sleep(0)
+if jogador == computador:
+    print('PARABENS! Você conseguiu me vencer!')
+else:
+    print('GANHEI! Pensei no {} e não no {}.'.format(computador, jogador))
+
 
