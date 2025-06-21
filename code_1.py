afd7c68a90650533d381fe881ddaf5eb2d5d8148
Arthur Caetano <arthur.cateano.da.silva01@gmail.com>
2025-06-21 18:54:23 -0300

    voltando

--- code_1.py
+++ code_1.py
@@ -1,10 +1,9 @@
 print('Hello Mundo')
 from random import randint
 from time import sleep
-computador = randint(0,6) # Faz o computador 'PENSAR'
+computador = randint(0,5) # Faz o computador 'PENSAR'
 print('-=-' * 20)
-print('Vou pensar em um número entre 0 a 6. Tente aidvinhar...')
-print('-=-' * 20)
+print('Vou pensar em um número entre 0 a 5. Tente aidvinhar...')
 jogador = int(input('Em que número pensei? ')) # Jogaodr tenta adivinhar
 print('PROCESSANDO...')
 sleep(0)
